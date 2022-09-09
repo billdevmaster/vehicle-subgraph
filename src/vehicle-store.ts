418c24aa881b854f2980c1b22ec2dd8d7cf73e24
billdevmaster71 <billdevmaster@gmail.com>
2022-09-10 00:00:18 +0300

    initial commit

--- src/vehicle-store.ts
+++ src/vehicle-store.ts
@@ -7,57 +7,17 @@ import {
   RoleRevoked,
   SetServiceWorkder
 } from "../generated/VehicleStore/VehicleStore"
-import { ExampleEntity } from "../generated/schema"
+import { Vehicle } from "../generated/schema"
 
 export function handleAddVehicle(event: AddVehicle): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.index = event.params.index
-  entity.owner = event.params.owner
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.SERVICE_WORKER_ROLE(...)
-  // - contract.VINToVehicle(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.getVehicle(...)
-  // - contract.hasRole(...)
-  // - contract.serviceWorkers(...)
-  // - contract.supportsInterface(...)
-  // - contract.vehicleToVIN(...)
-  // - contract.vehicles(...)
+  let vehicle = new Vehicle(event.params.index.toHex())
+  vehicle.owner = event.params.owner
+  vehicle.VIN = event.params.VIN
+  vehicle.name = event.params.name
+  vehicle.make = event.params.make
+  vehicle.model = event.params.model
+  vehicle.color = event.params.color
+  vehicle.save()
 }
 
 export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
